ada439c9596d6110f659c923cf2c36fff423c22d
frozenblackflame <15117265139@126.com>
2020-04-18 11:13:30 +0800

    添加文字显示自动换行

--- components/laboratory/leadership/LeaderPage.vue
+++ components/laboratory/leadership/LeaderPage.vue
@@ -149,6 +149,7 @@
     position: relative;
     top: 20px;
     text-align: left;
+    white-space:pre-wrap;
   }
   .time-style{
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
--- components/result/International/MainPage.vue
+++ components/result/International/MainPage.vue
@@ -148,6 +148,7 @@
     font-size: 15px;
     position: relative;
     top: 20px;
+    white-space:pre-wrap;
   }
   .time-style{
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
